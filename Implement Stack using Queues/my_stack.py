0a46e0a0a42ff80b1194250d38cef22c8eb13335
Tanya <morozt208@gmail.com>
2025-04-10 11:41:49 +0300

    sec commit, Implement Stack using Queues

---
+++ Implement Stack using Queues/my_stack.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class MyStack(object):
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.q.popleft()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.q[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.q
